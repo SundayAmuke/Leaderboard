ae2aa1b5cefd6e28b559ddb08967a7c7e9ce7256
Amuke Sunday <sundayamuke7@gmail.com>
2020-09-10 14:34:32 +0100

    Added an activity for project submission

---
+++ app/src/main/java/com/sundayamuke/leaderboard/ui/SubmitActivity.kt
@@ -0,0 +1,12 @@
+package com.sundayamuke.leaderboard.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sundayamuke.leaderboard.R
+
+class SubmitActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_submit)
+    }
+}
