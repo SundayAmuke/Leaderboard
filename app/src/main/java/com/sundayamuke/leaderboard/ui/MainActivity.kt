2669544bd57ea4d7756e9d5ebda685686db0aea7
Amuke Sunday <sundayamuke7@gmail.com>
2020-09-11 15:59:07 +0100

    Set the onClickListener to start SubmitActivity.kt
    Also did a bit of refactoring.

--- app/src/main/java/com/sundayamuke/leaderboard/ui/MainActivity.kt
+++ app/src/main/java/com/sundayamuke/leaderboard/ui/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.sundayamuke.leaderboard.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
-import androidx.viewpager2.widget.ViewPager2
-import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.sundayamuke.leaderboard.R
 import com.sundayamuke.leaderboard.adapters.ViewPagerAdapter
@@ -14,20 +13,16 @@ import java.lang.Exception
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var viewPager: ViewPager2
-    private lateinit var tabs: TabLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        viewPager = binding.viewpager
-        tabs = binding.tabs
 
         val adapter = ViewPagerAdapter(this)
-        viewPager.adapter = adapter
+        binding.viewpager.adapter = adapter
 
-        TabLayoutMediator(tabs, viewPager) { tab, position ->
+        TabLayoutMediator(binding.tabs, binding.viewpager) { tab, position ->
             tab.text = when (position) {
                 0 -> "Learning Leaders"
                 1 -> "Skill IQ Leaders"
@@ -37,5 +32,10 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        binding.txtSubmit.setOnClickListener {
+            val intent = Intent(this, SubmitActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
